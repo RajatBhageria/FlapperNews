dde8e3971a777cb7ed6904a737452ed677dc95cb
Rajat Bhageria <rajat1996@gmail.com>
2016-05-23 22:16:25 -0400

    Working...
    The up vote works…added links and added titles

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 var app = angular.module('flapperNews', []);
+
+app.factory('posts', [function(){
+	var o = {
+		posts: []
+	}
+	return o; 
+}]);
 app.controller('MainCtrl', [
 '$scope',
 function($scope){
@@ -11,10 +18,18 @@ function($scope){
 	];
 	$scope.addPost = function(){
 		if (!$scope.title || $scope.title === '') {return;}
-		$scope.posts.push({title: $scope.title, upvotes: 0});
+		$scope.posts.push({
+			title: $scope.title, 
+			upvotes: 0,
+			link: $scope.link
+		});
 		$scope.title='';
+		$scope.link='';
 	};
 	$scope.incrementUpvotes = function(post){
 		post.upvotes +=1;
 	};
+	$scope.decrementUpvotes = function(post){
+		post.upvotes -=1;
+	}
 }])
